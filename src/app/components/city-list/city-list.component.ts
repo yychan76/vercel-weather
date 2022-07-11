3d2a68ac4c178543ece9e737b55088540a162c3d
Chan Yew Yen <chan.yewyen@gmail.com>
2022-07-11 22:47:36 +0800

    Show weather in search results

--- src/app/components/city-list/city-list.component.ts
+++ src/app/components/city-list/city-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -6,17 +12,20 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { debounceTime, EMPTY, Observable, of, Subscription } from 'rxjs';
-import { GeocodeCity } from 'src/app/common/model';
+import { debounceTime, EMPTY, map, Observable, Subscription } from 'rxjs';
+import { GeocodeCity, Weather } from 'src/app/common/model';
 import { GeocoderService } from 'src/app/common/services/geocoder.service';
+import { WeatherService } from 'src/app/common/services/weather.service';
 
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CityListComponent implements OnInit {
-  cities: string[] = [
+  weatherCache: { [key: string]: Promise<Weather> } = {};
+  @Input() cities: string[] = [
     'My Location',
     'Singapore, SG',
     'Seoul, KR',
@@ -26,18 +35,22 @@ export class CityListComponent implements OnInit {
     'Washington, US',
   ];
   form!: FormGroup;
-  sub!: Subscription;
+  sub = new Subscription();
   citySearchResults!: Observable<GeocodeCity[]>;
 
   constructor(
     private fb: FormBuilder,
     private geocoderService: GeocoderService,
-    private router: Router
-  ) {}
+    private weatherService: WeatherService,
+    private router: Router,
+    private ref: ChangeDetectorRef
+  ) {
+    this.loadCities();
+    // this.loadWeatherForCities();
+  }
 
   ngOnInit() {
     this.createForm();
-    this.loadCities();
   }
 
   ngOnDestroy() {
@@ -49,16 +62,23 @@ export class CityListComponent implements OnInit {
       city: ['', [Validators.required, Validators.minLength(3)]],
     });
 
-    this.sub = this.form
-      .get('city')!
-      .valueChanges.pipe(debounceTime(1000))
-      .subscribe((query) => {
-        console.log('city search term: ', query);
-        if (query) {
-          this.citySearchResults = this.geocoderService.getCities(query);
-        } else {
-          this.citySearchResults = EMPTY;
-        }
+    this.sub.add(
+      this.form
+        .get('city')!
+        .valueChanges.pipe(debounceTime(1000))
+        .subscribe((query) => {
+          console.info('city search term: ', query);
+          if (query) {
+            this.citySearchResults = this.geocoderService.getCities(query);
+            this.getGlanceWeatherForSearchResults(this.citySearchResults);
+            // since we are not doing change detection always, check it when search value changes
+            this.ref.markForCheck();
+          } else {
+            this.citySearchResults = EMPTY;
+          }
+        })
+    );
+  }
 
   getCityStrWithGeoCode(city: GeocodeCity): string {
     return (
@@ -72,7 +92,27 @@ export class CityListComponent implements OnInit {
       city.lon
     );
   }
-      });
+
+  getGlanceWeatherForSearchResults(searchResults: Observable<GeocodeCity[]>) {
+    // console.info('getGlanceWeatherForSearchResults');
+    this.sub.add(
+      searchResults
+        .pipe(
+          map((cities) => {
+            cities.forEach((city) => {
+              const weatherSearchString = this.getCityStrWithGeoCode(city);
+              // console.info('weatherSearchString: ', weatherSearchString);
+              if (!this.weatherCache[weatherSearchString]) {
+                const weather = this.getWeatherPromise(weatherSearchString);
+                if (weather) {
+                  this.weatherCache[weatherSearchString] = weather;
+                }
+              }
+            });
+          })
+        )
+        .subscribe(() => { /* subscribe to call the observable */ })
+    );
   }
 
   getFlagEmoji(countryCode: string) {
@@ -115,6 +155,7 @@ export class CityListComponent implements OnInit {
   }
 
   loadCities() {
+    console.info('loadCities');
     let savedCities = localStorage.getItem('cities');
     if (savedCities) {
       JSON.parse(savedCities)
@@ -122,10 +163,35 @@ export class CityListComponent implements OnInit {
         .forEach((c: string) => {
           this.cities.push(c);
         });
+      console.info(`Loaded ${this.cities.length} cities`);
+    }
+  }
+
+  loadWeatherForCities() {
+    console.info('loadWeatherForCities');
+    let savedCities = localStorage.getItem('cities');
+    if (savedCities) {
+      JSON.parse(savedCities)
+        .filter((c: string) => this.cities.includes(c)) // only load weather for loaded cities
+        .forEach((c: string) => {
+          console.info(`loadWeather: ${c}`);
+          if (c != 'My Location') {
+            const weather = this.getWeatherPromise(c);
+            console.info('typeof weather: ', weather);
+            if (weather && !this.weatherCache[c]) {
+              this.weatherCache[c] = weather;
+            }
+          }
+        });
+      console.info(`Loaded weather for ${this.cities.length} cities`);
+      console.info(`weatherCache:`);
+      console.dir(this.weatherCache);
+      this.ref.markForCheck();
     }
   }
 
   displayCity(city: string) {
+    // console.info('displayCity:', city);
     let lat, lon;
     let geoTokens = city.split('_');
     if (geoTokens.length == 2) {
@@ -161,15 +227,23 @@ export class CityListComponent implements OnInit {
     return city;
   }
 
+  getWeatherPromise(city: string): Promise<Weather> {
+    // console.info('getWeatherPromise:', city);
+    // if (city == 'My Location') {
+    //   return {} as Weather;
+    // }
+    return this.weatherService.getCachedWeatherGlance(city);
+  }
+
   addCity(formDirective: FormGroupDirective) {
     // let city = this.form.value.city.trim().toLowerCase();
     let city = this.form.value.city.trim();
-    console.log(city);
+    console.info(city);
     if (!this.cities.includes(city)) {
       this.cities.push(city);
       localStorage.setItem('cities', JSON.stringify(this.cities));
     }
-    console.log(this.cities);
+    console.info(this.cities);
     this.form.reset();
     formDirective.resetForm();
   }
