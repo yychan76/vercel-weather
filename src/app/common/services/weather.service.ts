9184a29382073e9ab58c11ad0e22accd42eacb3a
Chan Yew Yen <chan.yewyen@gmail.com>
2022-07-11 22:40:14 +0800

    Support weather glance query

--- src/app/common/services/weather.service.ts
+++ src/app/common/services/weather.service.ts
@@ -1,22 +1,52 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Weather } from '../model';
 
 const URL_API_BASE = '/api/weather/v2/';
+const GLANCE_URL_API_BASE = '/api/weatherglance/';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  constructor(private http: HttpClient) {}
+  weatherCache: { [key: string]: any };
+  constructor(private http: HttpClient) {
+    this.weatherCache = {};
+  }
 
   getWeather(city: string): Observable<Weather[]> {
-    console.info("getWeather: ", city);
+    console.info('getWeather: ', city);
     return this.http.get<any>(this.getUrl(city));
   }
 
   getUrl(city: string) {
     return `${URL_API_BASE}${city}`;
+  }
+
+  getGlanceUrl(city: string) {
+    return `${GLANCE_URL_API_BASE}${city}`;
+  }
+
+  getWeatherGlance(city: string): Observable<Weather[]> {
+    let url = this.getGlanceUrl(city);
+    // console.info(`getWeatherGlance: ${city} ${url}`);
+    return this.http.get<any>(url);
+  }
+
+  async getCachedWeatherGlance(city: string): Promise<Weather> {
+    if (!this.weatherCache[city]) {
+      // cache the Promise
+      this.weatherCache[city] = firstValueFrom(this.getWeatherGlance(city));
+    }
+
+    try {
+      const weather = await this.weatherCache[city];
+      return weather[0];
+    } catch (err) {
+      delete this.weatherCache[city];
+      console.error(err);
+      return {} as Weather;
+    }
   }
 }
